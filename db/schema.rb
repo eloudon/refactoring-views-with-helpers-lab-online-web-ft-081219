9045877a2450efac20d4a36dd99326c647199882
Eric Loudon <loudon.eric@gmail.com>
2019-11-05 13:42:13 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -26,7 +25,5 @@ ActiveRecord::Schema.define(version: 20160126235547) do
     t.integer  "artist_id"
     t.index ["artist_id"], name: "index_songs_on_artist_id"
   end
-
-
 
 end
